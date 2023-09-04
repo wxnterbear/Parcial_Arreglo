f3b691af00e56847e5f4a38385cf3515ab10d4d9
wxnterbear <118340768+wxnterbear@users.noreply.github.com>
2023-09-04 12:44:33 -0500

    Add files via upload

---
+++ Arreglo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int a = 5;
+int b;
+int cont;
+
+int main(){
+	
+
+
+	for (int i = 0; i < a ; i++){
+		
+		cout << "Digite un numero: ";
+		cin >> b;
+			
+			
+		}
+		
+		for (int x = 0; x< a ; x++){
+			
+			for (int y = 0 ; y < a ; y++){
+				
+				x, y = cont;
+				cout << cont++;
+			}
+			
+		}
+		
+		
+			
+}	
+		
+		
+	/*/for (int x = 0 ; x < a ; x++){
+			
+			
+			cout << "posicion # " << x << ": " << b <<endl;
+	/*/	
